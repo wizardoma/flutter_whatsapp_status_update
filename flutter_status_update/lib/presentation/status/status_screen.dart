8a7203b15fdf7613a7abc2c5820635c051299543
Ibekason Alexander <alibekason@gmail.com>
2021-05-29 00:33:33 +0100

    bootstrap app screens

---
+++ flutter_status_update/lib/presentation/status/status_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StatusScreen extends StatefulWidget {
+  StatusScreen({Key key}) : super(key: key);
+
+  @override
+  _StatusScreenState createState() => _StatusScreenState();
+}
+
+class _StatusScreenState extends State<StatusScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
