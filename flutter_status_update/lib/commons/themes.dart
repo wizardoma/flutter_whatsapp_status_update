02d0c566122ed4e43a44460e2c2ffa5c284e4553
Ibekason Alexander <alibekason@gmail.com>
2021-05-30 16:24:41 +0100

    add theme data

---
+++ flutter_status_update/lib/commons/themes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_status_update/commons/colors.dart';
+
+var kTheme = ThemeData(
+  appBarTheme: AppBarTheme(
+    backgroundColor: kTealGreen,
+  ),
+
+);
--- flutter_status_update/lib/main.dart
+++ flutter_status_update/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_status_update/commons/themes.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: null,
+      theme: kTheme,
     );
   }
 }
