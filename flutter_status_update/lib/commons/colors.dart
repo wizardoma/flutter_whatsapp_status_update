8dacdf25b5620fc61a5a315ec18c331ac48ad1e3
Ibekason Alexander <alibekason@gmail.com>
2021-05-30 16:19:26 +0100

    add color constants

---
+++ flutter_status_update/lib/commons/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+const kTealGreen = Color(0xff128C7E);
+const kDarkTealGreen = Color(0xff075E54);
+const kLightGreen = Color(0xff25D366);
+const kBlue = Color(0xff34B7F1);
+
